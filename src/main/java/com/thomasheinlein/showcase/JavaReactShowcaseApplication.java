7fbd7bd8fb73eb7dd5501882b5c19c2d618d68ed
Thomas Heinlein <mail@thomasheinlein.com>
2022-12-04 16:38:00 +0100

    initial commit

---
+++ src/main/java/com/thomasheinlein/showcase/JavaReactShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.thomasheinlein.showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaReactShowcaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thomasheinlein/showcase/JavaReactShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasheinlein.showcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactShowcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
